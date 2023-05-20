5bc94f5009e894a0af61f255ae6f7e6ac8024b14
alaminkawsar <alaminkawsar8643@gmail.com>
2023-05-20 18:07:34 +0600

    Form submitting and navigate authentication

---
+++ HTML-FORM/authenticate.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Authentication</title>
+</head>
+<body>
+    <?php
+        $submitted = (int) isset($_POST['username']);
+
+        if(!$submitted){
+            echo "No information provided";
+        }else{
+            echo "<p>Your password information</p>";
+            echo "<ul>
+                    <li>username:".$_POST['username']."</li>"
+                    ."<li>password:".$_POST['password']."</li>"
+                ."</ul>";
+        }
+    ?>
+
+</body>
+</html>
